b3bb73b5f8524672c1aba7ed4ee235799d972bd6
Claudio Martins <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>
2022-08-19 09:27:48 -0300

    criação de models

---
+++ app/Models/WsProperties.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class WsProperties extends Model
+{
+    use HasFactory;
+}
